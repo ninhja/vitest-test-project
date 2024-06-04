fbbc4622ebd3f55ff312558ee5fe174c47c2e319
ana <ana@anniecannons.com>
2024-06-04 09:45:50 -0700

    added form name

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formName, setFormName] = useState("")
+
+  console.log(formName)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='main-div'>
+        <form className='form'>
+          <label id="label-id"> Enter your name </label>
+          <input className='input-name'
+           type="text" 
+           value={formName}
+           onChange={(e) => setFormName(e.target.value)}
+           />
+           
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        
     </>
   )
 }
